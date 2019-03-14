53b17c142a935439d84ac46d0d70884d156e2d71
Nero <nerochen25@berkeley.edu>
2019-03-13 22:04:37 -0700

    Add firstNonRepeatNum function

--- data_structures.js
+++ data_structures.js
@@ -1,8 +1,8 @@
 /////////////// Commonly asked Array interview questions//////////////
 // Find the second minimum element of an array
 const secondMin = arr => {
-    let min = Math.min(...arr);
-    let newArr = [];
+    let min = Math.min(...arr),
+        newArr = [];
 
     arr.forEach(el => {
         if (el > min) {
@@ -13,8 +13,35 @@ const secondMin = arr => {
     return Math.min(...newArr);
 }
 
-console.log(secondMin([1,2,2,0,0, -1]));
+// console.log(secondMin([1,2,2,0,0, -1]));
 
 // First non-repeating integers in an array
+//input: [1,2,3,5,1,2]
+//output: 1
+const firstNonRepeatNum = arr => {
+    let counter = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if (counter[arr[i]]) {
+            counter[arr[i]] += 1
+        } else {
+            counter[arr[i]] = 1;
+        }
+    }
+
+    let nonRepeatNums = Object.keys(counter).filter(el => {
+        return counter[el] === 1;
+    })
+    
+
+    for (let j = 0; j < arr.length; j++) {
+        if (nonRepeatNums.includes(arr[j].toString())) {
+            return arr[j];
+        }
+    }
+    return false;
+}
+
+console.log(firstNonRepeatNum([2,3,4,4,2,3,1,1,0]));
 // Merge two sorted arrays
 // Rearrange positive and negative values in an array
